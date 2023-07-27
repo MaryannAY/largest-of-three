3f10212c2061d3d2eb95f7b2edd392090cd4e2cf
MaryannAY <maryann.ahmed2@gmail.com>
2023-07-27 01:56:32 -0700

    The answer of the Js challenge

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+const Number1=100
+const Number2=200
+const Number3=300
+
+if(Number1<Number2 && Number2<Number3){
+    console.log("The largest number out of the three is 300")
+} else if (Number1>Number2 ||Number3===Number2){
+    console.log("The largest number out of the three is 300 ")
+} else console.log("the largest number out of the three is 300")
